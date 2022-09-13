0895644fd56cffecd4dbf88024454e8895149dc4
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-13 10:21:33 +0500

    Create an API endpoint that selects a random greeting

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def index
-    @message = Message.all.sample
-    render json: @messages
+    @message = Message.all.sample.greeting
+    render json: @message
   end
 end
