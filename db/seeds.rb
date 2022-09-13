6165623803f2f2aadb1941ac42373d04afd111e4
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-13 10:06:49 +0500

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+p "Seeding data..."
+p "Creating messages..."
+
+Message.create(greeting: "Hi there!")
+Message.create(greeting: "Morning!")
+Message.create(greeting: "It's good to see you!")
+Message.create(greeting: "What's up?")
+Message.create(greeting: "How are you?")
+
+p "Seeding data... DONE!"
+p "Created #{Message.count} messages!"
