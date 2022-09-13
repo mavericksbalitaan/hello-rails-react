6f9f63a4255b1f4df7e16e8adb14d7bd9ed20b4b
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-13 09:44:23 +0500

    Create App and Greeting components

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,15 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
+import '@hotwired/turbo-rails';
+import './controllers';
+import * as bootstrap from 'bootstrap';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
